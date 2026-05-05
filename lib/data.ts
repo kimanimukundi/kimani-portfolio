d9e7f440edcafad557c6750f8caeb10ea54acbc1
kimanimukundi <kimanimukundi605@gmail.com>
2026-05-05 10:10:15 +0300

    update contact info

--- lib/data.ts
+++ lib/data.ts
@@ -4,8 +4,7 @@ export const data = {
   tagline: ['SOFTWARE DEVELOPER', 'ML ENGINEER', 'PROBLEM SOLVER'],
   email: 'kimanimukundi605@gmail.com',
   phone: '+254 797 325 474',
-  github: 'github.com/kyatrixxkush',
-  github: 'github.com/kimanimukundi;',
+  github: 'github.com/kyatrixxkush', 'github.com/kimanimukundi;',
   location: 'Nairobi, Kenya',
   stats: [
     { num: '8+', label: 'Projects Shipped' },
